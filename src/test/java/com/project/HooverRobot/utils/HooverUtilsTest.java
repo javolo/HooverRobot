0c8faf2a440470c7b03de6c717f3fa41ece56a89
javolo <javo.osa@gmail.com>
2019-01-14 17:51:08 +0000

    Classes to do the rest part of it

--- src/test/java/com/project/HooverRobot/utils/HooverUtilsTest.java
+++ src/test/java/com/project/HooverRobot/utils/HooverUtilsTest.java
@@ -1,5 +1,21 @@
 package com.project.HooverRobot.utils;
 
-public class HooverUtilsTest {
+import org.junit.After;
+import org.junit.Before;
 
+public class HooverUtilsTest {
+	
+	private HooverUtils robotUtils;
+
+	@Before
+    public void setUp() {
+		robotUtils = new HooverUtils();
+    }
+
+    @After
+    public void tearDown() {
+    	robotUtils = null;
+    }
+    
+    
 }
